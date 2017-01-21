211732542302e69c6c73e253ee783aa749998a7c
Michał Adamczyk <michal.adamczyk@medapp.pl>
2017-01-21 19:36:32 +0100

    ActiveAndroid implemented

---
+++ app/src/main/java/pl/mihau/game/nindojo/Models/History.java
@@ -0,0 +1,11 @@
+package pl.mihau.game.nindojo.Models;
+
+import com.activeandroid.annotation.Table;
+
+/**
+ * Created by x on 21.01.2017.
+ */
+
+@Table(name = "History")
+public class History {
+}
